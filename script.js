649749d054f070d0257fb8046ebcc1411fa7fea1
Eduardo Fernandez Juarez <eduardo.fernandez@est.fi.uncoma.edu.ar>
2025-10-13 09:22:01 -0300

    por las dudas

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const zonaTarj = document.querySelector("#zona_tarj");
 
-const tarj = `<a href="index-artista.html" class="link_artista">
+const bd = "artistasBD.json";
+
+function crearTarjs(artistas){
+    zonaTarj.innerHTML = "";
+    artistas.forEach(art => {
+        const nombreArtista = art.nombre;
+        const link_img = art.imagen;
+
+        const tarj = `<a href="index-artista.html" class="link_artista">
                 <div class="tarj_art">
                     <p>${nombreArtista}</p>
                     <img src="${link_img}" alt="${nombreArtista}">
                 </div></a>`;
-
-
+    });
+}
 
