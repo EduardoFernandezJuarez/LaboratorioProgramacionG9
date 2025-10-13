6f38bccdf9e63b9bcd85fe0585bfc82ee09257c2
Eduardo Fernandez Juarez <eduardo.fernandez@est.fi.uncoma.edu.ar>
2025-10-12 23:47:19 -0300

    no habia metido el json a la carpeta (ahora si)

---
+++ script.js
@@ -0,0 +1,10 @@
+const zonaTarj = document.querySelector("#zona_tarj");
+
+const tarj = `<a href="index-artista.html" class="link_artista">
+                <div class="tarj_art">
+                    <p>${nombreArtista}</p>
+                    <img src="${link_img}" alt="${nombreArtista}">
+                </div></a>`;
+
+
+
